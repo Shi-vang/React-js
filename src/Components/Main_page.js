049f342816425ecf5e077578eff6fa080161ef23
shivang <shivanggarg48@gmail.com>
2022-03-18 18:06:16 +0530

    lastcommit

--- src/Components/Main_page.js
+++ src/Components/Main_page.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {connect} from 'react-redux'
 import {actionCreatorForBuyCake} from '../Redux/Cake/ActionCreator'
 const  Main_page=(props) =>{
 
   const navigate = useNavigate()
+  const [a,setA]=useState(0)
+  const [b,setB]=useState(0)
+
+  const funcc=()=>{
+    setB(b+1)
+    console.log("dcerff",b)
+  }
+
+  useEffect(() =>{
+    console.log('Hello use effect call ')
+
+    return (()=>{
+      console.log("component will unmount call")
+    })
+  },[])
 
   return (
       <>
       <h1>Welcome</h1>
       <p>number of cakes {props.numberOfCakes}</p>
       <button onClick={props.buyCake}>Buy cake</button>
+      <button onClick={()=>{setA(props.numberOfCakes);funcc()}}>Buy {a}</button>
       <button onClick={()=>navigate('/cart')}>nav to cart</button>
       </>
   );
